f347965fe7dcaffefb96e8ac3bf45d7acdf84bfe
rafern <rafern@protonmail.com>
2023-01-20 16:25:31 +0000

    Add Separator widget

---
+++ src/widgets/Separator.ts
@@ -0,0 +1,40 @@
+import { Spacing } from './Spacing';
+import type { SpacingProperties } from './Spacing';
+import type { Rect } from '../helpers/Rect';
+
+/**
+ * A {@link Spacing} with a colored background, where the color is the same as
+ * the theme's text color.
+ *
+ * @category Widget
+ * @category Alias Widget
+ */
+export class Separator extends Spacing {
+    /** Create a new Separator. */
+    constructor(properties?: Readonly<SpacingProperties>) {
+        super({
+            minWidth: 1,
+            minHeight: 1,
+            ...properties
+        });
+    }
+
+    protected override onThemeUpdated(property: string | null = null): void {
+        super.onThemeUpdated(property);
+
+        if(property === null || property === 'bodyTextFill') {
+            this.markWholeAsDirty();
+        }
+    }
+
+    protected override handlePainting(dirtyRects: Array<Rect>): void {
+        // Paint brackground
+        const ctx = this.viewport.context;
+        ctx.save();
+        ctx.fillStyle = this.bodyTextFill;
+        ctx.fillRect(this.x, this.y, this.idealWidth, this.idealHeight);
+        ctx.restore();
+
+        super.handlePainting(dirtyRects);
+    }
+}
