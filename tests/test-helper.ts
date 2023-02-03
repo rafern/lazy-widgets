847a6793d578b2a4afbbbe9cff0a7f6520bb2eb4
rafern <rafern@protonmail.com>
2023-02-03 11:16:11 +0000

    Add DOM API to test suite

---
+++ tests/test-helper.ts
@@ -0,0 +1,13 @@
+// similar to:
+// https://github.com/TypeStrong/ts-node/issues/55#issuecomment-186679305
+
+import { JSDOM } from 'jsdom';
+
+const jsdom = new JSDOM('');
+
+// setup global context without localStorage/sessionStorage support
+for (const key of Object.getOwnPropertyNames(jsdom.window)) {
+    if (!(key in global) && key !== 'localStorage' && key !== 'sessionStorage' && !key.startsWith('_')) {
+        (global as Record<string, unknown>)[key] = jsdom.window[key];
+    }
+}
