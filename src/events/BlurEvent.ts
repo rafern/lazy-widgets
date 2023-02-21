f3d3182fcf47512ec368eb4416f61d5d0e19e667
rafern <rafern@protonmail.com>
2023-02-21 17:35:31 +0000

    Added BlueEvent and FocusEvent, but no functionality yet

---
+++ src/events/BlurEvent.ts
@@ -0,0 +1,26 @@
+import type { FocusType } from '../core/FocusType';
+import { StickyEvent } from './StickyEvent';
+
+/**
+ * A sticky event that is fired when a {@link Widget} loses a specific
+ * {@link FocusType | focus}. Not capturable by user listeners or the widget
+ * receiving the event.
+ *
+ * Always check the focus type to avoid weird behaviour.
+ */
+export class BlurEvent extends StickyEvent {
+    static override readonly type = 'blur';
+    override readonly type: typeof BlurEvent.type;
+    override readonly userCapturable: false;
+
+    /** The gained focus type */
+    readonly focusType: FocusType;
+
+    constructor(focusType: FocusType) {
+        super();
+
+        this.type = BlurEvent.type;
+        this.userCapturable = false;
+        this.focusType = focusType;
+    }
+}
---
+++ src/events/FocusEvent.ts
@@ -0,0 +1,26 @@
+import type { FocusType } from '../core/FocusType';
+import { StickyEvent } from './StickyEvent';
+
+/**
+ * A sticky event that is fired right before a {@link Widget} gains a specific
+ * {@link FocusType | focus}. Not capturable by user listeners. If not captured
+ * by the widget, the widget will not gain the focus.
+ *
+ * Always check the focus type to avoid weird behaviour.
+ */
+export class FocusEvent extends StickyEvent {
+    static override readonly type = 'focus';
+    override readonly type: typeof FocusEvent.type;
+    override readonly userCapturable: false;
+
+    /** The gained focus type */
+    readonly focusType: FocusType;
+
+    constructor(focusType: FocusType) {
+        super();
+
+        this.type = FocusEvent.type;
+        this.userCapturable = false;
+        this.focusType = focusType;
+    }
+}
