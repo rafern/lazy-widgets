a797a85fedbc9b9c9ecf28494ad9582f99286f47
rafern <rafern@protonmail.com>
2023-04-19 17:31:40 +0100

    Simpler event initialization (incomplete, need to change other events)

--- src/events/BlurEvent.ts
+++ src/events/BlurEvent.ts
@@ -11,17 +11,13 @@ import type { FocusType } from '../core/FocusType.js';
  */
 export class BlurEvent extends StickyEvent {
     static override readonly type = 'blur';
-    override readonly type: typeof BlurEvent.type;
-    override readonly userCapturable: false;
+    override readonly type = BlurEvent.type;
+    override readonly userCapturable = false;
 
-    /** The gained focus type */
-    readonly focusType: FocusType;
-
-    constructor(focusType: FocusType) {
+    /**
+     * @param focusType - The gained focus type
+     */
+    constructor(readonly focusType: FocusType) {
         super();
-
-        this.type = BlurEvent.type;
-        this.userCapturable = false;
-        this.focusType = focusType;
     }
 }
