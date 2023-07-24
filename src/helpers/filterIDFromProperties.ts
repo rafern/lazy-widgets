47638ae1bc4caa75a62f5a8c0351af2cee05d744
rafern <rafern@protonmail.com>
2023-07-24 14:53:05 +0100

    Filter `enabled` property

--- src/helpers/filterIDFromProperties.ts
+++ src/helpers/filterIDFromProperties.ts
@@ -1,11 +1,11 @@
 import { WidgetProperties } from '../widgets/Widget.js';
 export function filterIDFromProperties(properties?: WidgetProperties): WidgetProperties | undefined {
-    if (properties === undefined || !('id' in properties)) {
+    if (properties === undefined || !(('id' in properties) || ('enabled' in properties))) {
         return properties;
     } else {
         const newProperties: Record<string, unknown> = {};
         for (const name of Object.getOwnPropertyNames(properties)) {
-            if (name !== 'id') {
+            if (name !== 'id' && name !== 'enabled') {
                 newProperties[name] = (properties as Record<string, unknown>)[name];
             }
         }
